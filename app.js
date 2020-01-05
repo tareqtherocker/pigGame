0fd61186f5fdb7537cd94fd2b9d9e51c0a6a0d87
Tareq <tareqtherocker@gmail.com>
2020-01-06 02:19:44 +0600

    used querySelector to manipulate

--- app.js
+++ app.js
@@ -16,5 +16,8 @@ scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
+dice = Math.floor(Math.random() * 6) + 1; //created a random number
+
+//DOM manipulation in two ways
+document.querySelector('#current-' + activePlayer).textContent = dice; //textContent will only be manipulated only for text.active
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'; //textContent will only be manipulated only for text.active
