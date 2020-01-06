855a6580c9bcde65c54eefea928a04c17e0db3cf
Tareq <tareqtherocker@gmail.com>
2020-01-06 22:11:15 +0600

    changed background

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
         //check if player won the game
-        if (scores[activePlayer] >= 10) {
+        if (scores[activePlayer] >= 100) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!!';
             document.querySelector('.dice').style.display = 'none';
 
